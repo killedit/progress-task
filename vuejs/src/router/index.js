f385dc354ff2c96073ea7cae317894aa7fd4b1a2
tihomir <tihomir.petkov@abv.bg>
2025-11-01 19:56:07 +0200

    Docker build fails on WSL, removed route in vuejs

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -9,7 +9,6 @@ const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
-  { path: '/tasks', name: 'Tasks', component: Tasks, meta: { requiresAuth: true }},
   { path: '/tasks/create', component: TaskForm },
   { path: '/tasks/:id/edit', component: TaskForm, props: true },
   { path: '/register', name:'Register', component: Register },
