c2448d05e34f11d00458b9c2e7a5699f95c6b1b7
tihomir <tihomir.petkov@abv.bg>
2025-11-11 12:58:15 +0200

    Home page datetime formatting fix

--- vuejs/src/views/Home.vue
+++ vuejs/src/views/Home.vue
@@ -123,7 +123,16 @@ const changePage = (page) => {
 }
 
 const formatDate = (dateString) => {
-	return new Date(dateString).toLocaleString()
+  if (!dateString) return ''
+  const date = new Date(dateString)
+  return date.toLocaleString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  })
 }
 
 const addTask = () => {
